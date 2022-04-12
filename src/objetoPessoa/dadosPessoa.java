4b6a181bd9ca04e1eedee520763c99f9f865ec29
Davi Paulino <davipm@hotmail.com>
2022-04-12 17:35:55 -0300

    impasse

--- src/objetoPessoa/dadosPessoa.java
+++ src/objetoPessoa/dadosPessoa.java
@@ -1,7 +1,16 @@
 package objetoPessoa;
 
-public class dadosPessoa {
-		public int idade;
-		public String nome;
-		public String sobreNome;
+public class dadosPessoa{
+		public int[] idades;
+		public String[] nomes;
+		public String[] sobreNomes;
+		
+		void dado(){
+			System.out.println("Nome: "+ nomes+sobreNomes +"\n"
+					+"Idade: "+String.format("", idades));
+		}
+		public String toString (){
+			return "Nome: "+ nomes+sobreNomes +"\n"
+					+"Idade: "+String.format("", idades);
+		}
 }
--- src/objetoPessoa/sistemaNomeLista.java
+++ src/objetoPessoa/sistemaNomeLista.java
@@ -1,37 +1,32 @@
 package objetoPessoa;
 
-import java.util.Scanner;
 
 public class sistemaNomeLista {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
 		dadosPessoa pessoa=new dadosPessoa();
+		pessoa.nomes=new String[3];
+		pessoa.sobreNomes=new String[3];
+		pessoa.idades=new int[3];
 		
-		String[] nomes=new String[3];
+		pessoa.nomes[0]= "José";
+		pessoa.sobreNomes[0]= " da Silva";
+		pessoa.idades[0]=35;
 		
-		System.out.print("Manda seu nome aí: ");
-		pessoa.nome=sc.next();
-		System.out.print("Manda seu sobrenome: ");
-		pessoa.sobreNome=sc.next();
-		System.out.print("Manda sua idade: ");
-		pessoa.idade=sc.nextInt();
+		pessoa.nomes[1]= "Tom";
+		pessoa.sobreNomes[1]= " Jobim";
+		pessoa.idades[1]=67;
 		
-		System.out.println("Nome: "+pessoa.nome);
-		System.out.println("Sobrenome: "+pessoa.sobreNome);
-		System.out.println("Idade: "+pessoa.idade);
+		pessoa.nomes[2]= "Ruby";
+		pessoa.sobreNomes[2]= " Rubynson";
+		pessoa.idades[2]=14;
 		
-		sc.nextLine();
 		for(int i=0;i<3;i++) {
-			System.out.print("Manda seu nome aí: ");
-			nomes[i]=sc.nextLine();
+			System.out.println("Nome: " + pessoa.nomes[i] + pessoa.sobreNomes[i]);
+			System.out.println("Idade: " + pessoa.idades[i]);
 		}
 		
-		for(String nome:nomes) {
-			System.out.println(nome);
-		}
-		
-		sc.close();
+		pessoa.dado();
 
 	}
 
